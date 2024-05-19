bea39f9e00246cda7a11741041087b32ce605d07
sebas <sebastian.londono75@correo.tdea.edu.co>
2024-05-19 18:58:10 -0500

    CHG homepage for search category and username

--- src/homePage/components/ProductGrind.jsx
+++ src/homePage/components/ProductGrind.jsx
@@ -2,7 +2,8 @@ import { ProductCard } from "./ProductCard";
 import { ProductContext } from '../../products/context'
 import { useContext, useEffect, useState } from "react";
 
-export const ProductGrid = ({ product }) => {
+export const ProductGrid = ({ product , tittle}) => {
+
   const [currentPage, setCurrentPage] = useState(1);
   const productsPerPage = 4;
 
@@ -18,7 +19,7 @@ export const ProductGrid = ({ product }) => {
 
   return (
     <div className="container">
-      <h3 className="mt-3">Últimos productos</h3>
+      <h3 className="mt-3">{tittle}</h3>
       <div className="row">
         {currentProducts.map((product) => (
           <div key={product.id} className="col-md-6 mb-3">
